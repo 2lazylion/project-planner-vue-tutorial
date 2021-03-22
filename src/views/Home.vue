4eff7f0681e52ca9e14f7d64628281570987c249
2lazylion <e_cadet22@hotmail.com>
2021-03-22 16:05:28 -0400

    tuto done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,57 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="currentFilter = $event" :currentFilter="currentFilter"/>
+    <div v-if="filteredProjects.length">
+      <div v-for="project in filteredProjects" :key="project.id">
+          <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import SingleProject from '../components/SingleProject.vue'
+import FilterNav from '../components/FilterNav.vue'
 
 export default {
   name: 'Home',
   components: {
+    SingleProject, FilterNav
+  },
+  data() {
+    return { 
+      projects: [],
+      currentFilter: 'all'
+    }
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter(project => project.id !== id)
+    },
+    handleComplete(id) {
+      let p = this.projects.find(project => project.id === id)
+      p.complete = !p.complete
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err))
+  },
+  computed: {
+    filteredProjects() {
+      if(this.currentFilter === 'completed'){
+        return this.projects.filter(project => project.complete)
+      }
+      else if(this.currentFilter === 'ongoing') {
+        return this.projects.filter(project => !project.complete)
+      }
+      else {
+        return this.projects
+      }
+    }
   }
 }
 </script>
